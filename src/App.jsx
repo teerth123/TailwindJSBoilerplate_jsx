df00071bf20b7b2ccd398b741b517562092f26fb
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-03-15 12:46:22 +0530

    boilerplate for TAILWINDCSS + JS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-red-400 underline'>Red underlined text</h1>
+    </>
+  )
+}
+
+export default App
